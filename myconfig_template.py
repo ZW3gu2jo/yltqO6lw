020bd2cd286a413efaf6647af8be841a310bc4ad
ZW3gu2jo <k2r8hms6v@outlook.com>
2018-11-15 14:37:57 +0800

    add cfg tmp

--- myconfig_template.py
+++ myconfig_template.py
@@ -4,7 +4,10 @@
 proxyHost = "http-dyn.abuyun.com"
 proxyPort = "9020"
 
-
-# 代理隧道验证信息 HTTP 动态版
+# 代理隧道验证信息
 proxyUser = ""
 proxyPass = ""
+
+#具体投票页面链接
+voteurl = 'http://m.winshang.com/h5/2018xnfh/xmvote.aspx'
+votejs = "addvote()"
